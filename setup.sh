157d4e5665934c4e243d885bd8e2e49bec074f9a
Cédric HERZOG <cedric.herzpg@codeclarity.io>
2025-04-04 13:51:47 +0200

    Remove make usage

--- setup.sh
+++ setup.sh
@@ -11,11 +11,6 @@ if ! command -v openssl &>/dev/null; then
     exit 1
 fi
 
-if ! command -v make &>/dev/null; then
-    echo "Error: make is not installed."
-    exit 1
-fi
-
 if ! command -v docker &>/dev/null || ! command -v docker-compose &>/dev/null; then
     echo "Error: docker or docker-compose is not installed."
     exit 1
@@ -27,37 +22,46 @@ git clone git@github.com:CodeClarityCE/deployment.git
 cd deployment
 
 # openssl needs to be installed
-make setup-jwt
+mkdir -p jwt
+openssl ecparam -name secp521r1 -genkey -noout -out jwt/private.pem
+openssl ec -in jwt/private.pem -pubout -out jwt/public.pem
 
-read -p "Is this installation local (y/n)? " local_install
+read -p "Is this installation running on localhost (Y/n)? " local_install
 
 domain_name="localhost"
-if [[ "$local_install" == "y" || "$local_install" == "Y" ]]; then
-    echo "Running local installation."
-else
+if [[ "$local_install" == "n" || "$local_install" == "N" ]]; then
     read -p "Enter the domain name (localtest.io): " domain_name
     echo "Running installation for domain: $domain_name"
     sed -i '' "s/WEB_HOST=https:\/\/localhost/WEB_HOST=https:\/\/$domain_name/" .env.api
     sed -i '' "s/SERVER_NAME=localhost/SERVER_NAME=$domain_name/" .env.frontend
-    read -p "Do you want Caddy to generate certificates (y/n)? " caddy_generate_certs
+    read -p "Do you want Caddy to generate certificates (Y/n)? " caddy_generate_certs
 
-    if [[ "$caddy_generate_certs" == "y" || "$caddy_generate_certs" == "Y" ]]; then
-        echo "Letting Caddy generate certificates..."
-        # Caddy will handle certificate generation automatically
-    else
+    if [[ "$caddy_generate_certs" == "n" || "$caddy_generate_certs" == "N" ]]; then
         echo "Generating self-signed certificates..."
         mkdir -p certs
         openssl req -x509 -newkey rsa:4096 -keyout certs/tls.key -out certs/tls.pem -days 365 -nodes -subj "/CN=$domain_name"
         sed -i '' 's/# - .\/certs:\/etc\/caddy\/certs:ro/- .\/certs:\/etc\/caddy\/certs:ro/' docker-compose.yaml
         sed -i '' 's/# CADDY_SERVER_EXTRA_DIRECTIVES=/CADDY_SERVER_EXTRA_DIRECTIVES=/' .env.frontend
+    else
+        echo "Letting Caddy generate certificates..."
     fi
+else
+    echo "Running local installation."
 fi
 
-# curl and docker compose need to be installed
-make up
-make download-dumps
-make knowledge-setup
-make restore-database
-make down && make up
+# Start containers
+docker compose -f docker-compose.yaml up -d
+# Download dumps
+sh scripts/download-dumps.sh
+
+docker compose -f docker-compose.yaml -f docker-compose.knowledge.yaml run --rm knowledge -knowledge -action setup
+
+cd scripts && sh restore-db.sh codeclarity
+cd scripts && sh restore-db.sh knowledge
+cd scripts && sh restore-db.sh config
+cd scripts && sh restore-db.sh plugins
+
+docker compose -f docker-compose.yaml down
+docker compose -f docker-compose.yaml up -d
 
 echo "Installation successful, you can now visit: https://$domain_name:443"
