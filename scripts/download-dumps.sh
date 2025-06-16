6f241d5d2d564a72acca7144d676e9cc4dbaf0e7
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-06-16 09:23:17 +0200

    Update dump download script

--- scripts/download-dumps.sh
+++ scripts/download-dumps.sh
@@ -1,5 +1,5 @@
 cd dump
-curl https://drive.codeclarity.io/s/qRq4eRe8LBzbzXG/download/knowledge.dump --output knowledge.dump
-curl https://drive.codeclarity.io/s/rK4mEQnZxfgB4FS/download/config.dump --output config.dump
-curl https://drive.codeclarity.io/s/EHNio4djbFdrTP8/download/codeclarity.dump --output codeclarity.dump
+curl -L https://drive.codeclarity.io/s/qRq4eRe8LBzbzXG/download/knowledge.dump --output knowledge.dump
+curl -L https://drive.codeclarity.io/s/rK4mEQnZxfgB4FS/download/config.dump --output config.dump
+curl -L https://drive.codeclarity.io/s/EHNio4djbFdrTP8/download/codeclarity.dump --output codeclarity.dump
 cd -
